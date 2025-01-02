658e16ebfe88a69c7844268c42970094d636b323
Abyansyah <ahmadaby66@gmail.com>
2025-01-02 20:43:45 +0700

    Test Commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,6 @@ export const Hero = () => {
       <div className="flex flex-col gap-y-4">
         <h1 className="text-center text-3xl items-center font-play font-medium">photolyphia.</h1>
         <p className="text-sm text-center">STOCK PHOTOS</p>
-        
       </div>
     </div>
   );
